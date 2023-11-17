9b7bd4599fa4cbd895074506eae671cf4b996da6
Tran Dinh Toan <trandinhtoan2610@gmail.com>
2023-11-17 14:29:42 +0700

    login register

---
+++ src/main/java/Controllers/RegisterController.java
@@ -0,0 +1,91 @@
+
+
+package Controllers;
+
+import DAO.ProfileDAO;
+import Models.Profile;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+/**
+ *
+ * @author trand
+ */
+@WebServlet(name = "RegisterController", urlPatterns = {"/register"})
+public class RegisterController extends HttpServlet {
+
+   
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+            response.setContentType("text/html;charset=UTF-8");
+            String username = request.getParameter("username");
+            String password = request.getParameter("password");
+            String surname = request.getParameter("surname");
+            String firstname = request.getParameter("firstname");
+            String imgAvatar = request.getParameter("imgAvatar");
+            String dateBirth = request.getParameter("date");
+            String genderString = request.getParameter("gender");
+            String phoneNumber = request.getParameter("phoneNumber");
+            String email = request.getParameter("email");
+            
+            Profile p = ProfileDAO.selectByUsername(username);
+            if(p == null){
+                try {
+               
+                    java.sql.Date dateBirt = java.sql.Date.valueOf(dateBirth);
+
+             
+                    int gender = Integer.parseInt(genderString);
+
+               
+                    Profile profile = new Profile();
+                    profile.setUsername(username);
+                    profile.setPassword(password);
+                    profile.setSurname(surname);
+                    profile.setFirstname(firstname);
+                    profile.setImgAvatar(imgAvatar);
+                    profile.setDateBirth(dateBirt);
+                    profile.setGender(gender);
+                    profile.setPhonenumber(phoneNumber);
+                    profile.setEmail(email);
+                
+                    ProfileDAO profileDAO = new ProfileDAO();
+                    profileDAO.createProfile(profile);              
+                } catch (Exception e) {
+                
+                    System.out.println("Loi: " + e);
+                }
+            }else {
+                request.setAttribute("message","Tài khoản đã tồn tại");
+            }
+        request.getRequestDispatcher("/views/register.jsp").forward(request, response);
+
+        }
+    
+
+
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+   
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ src/main/webapp/views/login.jsp
@@ -0,0 +1,109 @@
+<%-- 
+    Document   : sign_in
+    Created on : Nov 10, 2023, 1:36:56 PM
+    Author     : Admin
+--%>
+
+
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Đăng nhập</title>
+    <link rel="stylesheet" href="css/sign_in.css">
+    <link rel="stylesheet" href="${pageContext.request.contextPath}/views/css/style.css">   
+    
+    <style>
+    body {
+    font-family: Arial, sans-serif;
+    background-image: url('nen.jpg'); /* Thay đổi 'background.jpg' thành đường dẫn hoặc tên tập tin ảnh của bạn */
+    background-size: cover;
+    background-position: center;
+    margin: 0;
+    padding: 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+    }
+
+form {
+    background-color: rgba(255, 255, 255, 0.8); /* Màu nền với độ trong suốt */
+    border-radius: 8px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    padding: 20px;
+    width: 300px;
+    text-align: center;
+}
+
+h2 {
+    margin-bottom: 20px;
+}
+
+label {
+    display: block;
+    margin-bottom: 8px;
+}
+
+input {
+    width: 100%;
+    padding: 8px;
+    margin-bottom: 12px;
+    box-sizing: border-box;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+}
+
+button {
+    background-color: #4caf50;
+    color: #fff;
+    padding: 10px;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+    width: 100%;
+}
+
+button:hover {
+    background-color: #45a049;
+}
+
+.signup-link {
+    margin-top: 10px;
+    font-size: 14px;
+    color: #333;
+}
+
+.signup-link a {
+    color: #4caf50;
+    text-decoration: none;
+}
+
+.signup-link a:hover {
+    text-decoration: underline;
+}
+    </style>
+    
+</head>
+<body>
+    
+    
+    <form method="POST" action="/socialmedia_j2ee/login">
+        <h2>Đăng nhập</h2>
+        
+        <input type="text" id="username" name="username" placeholder="Tài khoản" >
+      
+        <input type="password" id="password" name="password" placeholder="Mật khẩu" >
+
+        <button type="submit">Đăng nhập</button>
+
+        <div class="signup-link">
+            Chưa có tài khoản? <a href="register.jsp">Đăng ký ngay</a>
+        </div>
+    </form>
+    <h2 style="color: red"> ${error}</h2>
+</body>
+</html>
