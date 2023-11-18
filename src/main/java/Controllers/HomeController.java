b79a674a74623d83402bd24488e5afbad1376ed6
Hikiyoshi <datduong4869@gmail.com>
2023-11-18 11:07:15 +0700

    change name of class Home

--- src/main/java/Controllers/HomeController.java
+++ src/main/java/Controllers/HomeController.java
@@ -10,15 +10,18 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpSession;
+import Models.Profile;
 
 /**
  *
  * @author Admin
  */
 @WebServlet(name = "Home", urlPatterns = {"/"})
-public class Home extends HttpServlet {
+public class HomeController extends HttpServlet {
  @Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.getRequestDispatcher("/views/index.jsp").forward(request, response);
+            HttpSession session = request.getSession();
+            request.getRequestDispatcher("/views/index.jsp").forward(request, response);
 	}
 }
